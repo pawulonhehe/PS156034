eb5c0003663a2103cb9202d6b10a1af5ae395990
pawulonhehe <58747660+pawulonhehe@users.noreply.github.com>
2020-03-20 12:00:01 +0100

    Add files via upload

---
+++ Lekcja 1/zadania z lekcji pierwszej.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    /*
+    //1_2_1
+    printf("Hello World\n");
+    //1_2_2
+    printf("Bardzo\ndlugi\nnapis\n");
+    //1_2_3
+    printf("Napis zawierajacy rozne dziwne znaczki // \\ \\ $ \& \%\n");
+    //1_2_4
+    int number;
+    scanf("%i", &number);
+    printf("%i", number);
+    //1_2_5
+    float number;
+    scanf("%f", &number);
+    printf("%f", number);
+
+    //1_2_6
+    int number1, number2, number3;
+    scanf("%i %i %i", &number1, &number2, &number3);
+    printf("%i\n%i\n%i\n", number1, number2, number3);
+    //1_2_7
+    int number;
+    scanf("%i", &number);
+    printf("%i", number+1);
+    //1_2_8
+    int n1, n2, n3;
+    scanf("%i %i %i", &n1, &n2, &n3);
+    printf("%f",(float)(n1+n2+n3)/3);
+
+    //1_2_9
+    float x;
+    scanf("%f", &x);
+    printf("%f", sqrt(x));
+    //1_2_10
+
+    float x;
+    scanf("%f", &x);
+    printf("%f", fabs(x));
+    //1_2_11
+
+    float n;
+    scanf("%f",&n);
+    printf("%.2f",n);
+     */
+    //1_2_12
+
+}
